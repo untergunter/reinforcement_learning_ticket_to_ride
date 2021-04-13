20d1dece4bf85a9d83a6bc571d565b1a818bfdaa
untergunter <unteman92@gmail.com>
2021-04-13 18:36:28 +0300

    test_calculate_all_valid_ways_to_get_grey_road

--- TicketToRideGame.py
+++ TicketToRideGame.py
@@ -2,7 +2,8 @@ from random import shuffle
 
 import numpy as np
 import pandas as pd
-from collections import defaultdict
+from collections import defaultdict, namedtuple
+
 
 class TicketToRideGame:
 
@@ -20,35 +21,101 @@ class TicketToRideGame:
     def create_missions_deck(self):
         missions = pd.read_csv(r'data/destinations.csv')
         tuple_per_mission = missions.to_records(index=False)
-        tuple_dict_count_for_Deck = {mission:1 for mission in tuple_per_mission}
+        tuple_dict_count_for_Deck = {mission: 1 for mission in tuple_per_mission}
         missions_deck = Deck(tuple_dict_count_for_Deck)
         return missions_deck
 
 
 class Board:
     def __init__(self):
-        self.road_id_by_name,self.road_owner = self.setup_roads()
-
+        self.road_data_by_id, self.road_owner, self.road_id_by_name \
+            = self.setup_roads()
 
     def setup_roads(self):
         roads = pd.read_csv(r'data/Railroads.csv')
-        path_id_by_city_names = defaultdict(list)
+        Road = namedtuple('Road', ['length', 'color'])
+        road_data_by_id = dict()
+        road_id_by_name = defaultdict(list)
         path_occupied_id = [None for player_id in range(len(roads))]
         for route_id, row in roads.iterrows():
-            route_name = (row[0],row[1])
-            path_id_by_city_names[route_name].append()
-        return path_id_by_city_names,path_occupied_id
+            route_name = (row[0], row[1])
+            road_data = Road(length=row[2], color=row[3])
+            road_data_by_id[route_id]= road_data
+            road_id_by_name[route_name].append(route_id)
+        return road_data_by_id, path_occupied_id, road_id_by_name
+
+    def generate_valid_cards_count_for_path(self
+                                            , length_needed: int
+                                            , color_name: str
+                                            , color_amount: int
+                                            , joker_amount: int):
+        """
+        we prefer to use color and not jokers if possible.
+        :param length_needed:
+        :param color_name:
+        :param color_amount:
+        :param joker_amount:
+        :return:
+        """
+        if color_amount >= length_needed:
+            return (color_name , length_needed)
+        elif color_amount < length_needed:
+            enough_jokers = length_needed - color_amount
+            if enough_jokers <= joker_amount:
+                if color_amount>0:
+                    return (color_name,color_amount
+                        , 'joker',enough_jokers)
+                elif color_amount == 0:
+                    return ('joker', enough_jokers)
+            else:
+                return None
+
+    def calculate_all_valid_ways_to_get_grey_road(self, cards, length):
+        all_valid_combinations = {self.cards_specific_color(color, length, cards)
+                                  for color, amount in cards.items()}
+        all_valid_combinations.discard(None)
+        if len(all_valid_combinations) == 0:
+            return None
+        elif len(all_valid_combinations) > 0:
+            return all_valid_combinations
+
+    def cards_can_purchase_road(self, cards: dict, road_id: int):
+        length, route_color = self.road_data_by_id[road_id]
+        if route_color == 'gray':
+            all_options = self.calculate_all_valid_ways_to_get_grey_road\
+                (self, cards, length)
+            return all_options
+
+        elif route_color != 'gray':
+            cards_for_route = \
+                self.cards_specific_color(route_color, length, cards)
+            if cards_for_route is not None:
+                all_options = [cards_for_route]
+                return all_options
+
+    def cards_specific_color(self, route_color: str, length: int, cards: dict):
+        jokers_available = 0 if not 'joker' in cards else cards['joker']
+        color_cards_count = 0 if not route_color in cards else cards[route_color]
+
+        cars_combination = self.generate_valid_cards_count_for_path(
+            length, route_color, color_cards_count, jokers_available)
+
+        return cars_combination
+
+    def can_a_path_be_occupied(self, cards: dict, road_id: int):
+        road_is_not_occupied = self.road_owner[road_id] is None
+
 
 class Deck:
     """
     this is a deck of cards
     """
-    def __init__(self,type_quantity:dict):
+
+    def __init__(self, type_quantity: dict):
         self.type_quantity = type_quantity
         self.next_deck = []
         self.current_deck = []
         self.reset_deck()
-
 
     def reset_deck(self):
         """
@@ -58,20 +125,20 @@ class Deck:
         :return: None
         """
 
-        if len(self.next_deck)!=0:
+        if len(self.next_deck) != 0:
             self.current_deck = self.next_deck.copy()
             self.next_deck = []
 
         else:
             current_deck = []
-            for card,number_of in self.type_quantity.items():
+            for card, number_of in self.type_quantity.items():
                 for _ in range(number_of):
                     current_deck.append(card)
 
             shuffle(current_deck)
             self.current_deck = current_deck
 
-    def pop_n(self,n:int):
+    def pop_n(self, n: int):
         """
         removes n cards from the current deck and returns them
 
@@ -79,14 +146,14 @@ class Deck:
         :return:
         """
 
-        result=[]
+        result = []
         for _ in range(n):
-            if len(self.current_deck)==0:
+            if len(self.current_deck) == 0:
                 self.reset_deck()
             result.append(self.current_deck.pop())
         return result
 
-    def add_cards_underneath(self,cards_back_to_deck):
+    def add_cards_underneath(self, cards_back_to_deck):
         """
         returns the cards to the next deck
         :param cards_back_to_deck:
@@ -96,9 +163,11 @@ class Deck:
         for card in cards_back_to_deck:
             self.next_deck.append(card)
 
+
 class Player:
     def __init__(self):
         pass
+
 
 if __name__ == '__main__':
     pass
--- testTicketToRideGame.py
+++ testTicketToRideGame.py
@@ -1,28 +1,80 @@
 import pytest
-from collections import defaultdict
+from collections import defaultdict,Counter
+from TicketToRideGame import Deck, Board
 
-class test_Deck():
-    from TicketToRideGame import Deck
-    deck_input={'a':1,'b':2,'c':3}
-    test_deck = Deck(deck_input)
 
-    assert test_deck.type_quantity == deck_input
-    all_elements = len(test_deck.current_deck)
-    elements = test_deck.pop_n(all_elements)
+deck_input = {'a':1,'b':2,'c':3}
+
+@pytest.fixture()
+def deck():
+    return Deck(deck_input)
+
+def test_init(deck):
+    assert deck.next_deck == []
+    input_from_current_deck = dict(Counter(deck.current_deck))
+    assert input_from_current_deck == deck_input
+
+def test_deck(deck):
+    assert deck.type_quantity == deck_input
+
+def test_deck_pop_n(deck):
+    all_elements = len(deck.current_deck)
+    elements = deck.pop_n(all_elements)
     result_dict = defaultdict(int)
     for element in elements:
         result_dict[element]+=1
     assert dict(result_dict) == deck_input
+    assert len(deck.current_deck) == 0
+    deck.reset_deck()
+    elements = deck.pop_n(all_elements+1)
+    assert len(deck.current_deck) == all_elements-1
 
-
-    #make sure the restart mechanisem works
-    elements = test_deck.pop_n(all_elements)
-    result_dict = defaultdict(int)
-    for element in elements:
-        result_dict[element] += 1
-    assert dict(result_dict) == deck_input
+def test_reset_deck(deck):
+    deck.current_deck = []
+    deck.reset_deck()
+    input_copy = deck_input.copy()
+    for card in deck.current_deck:
+        input_copy[card] -= 1
+    for card,count in input_copy.items():
+        assert count == 0
 
     # test add_cards_underneath
-    assert 'new_card' not in test_deck.current_deck
-    test_deck.add_cards_underneath(('new_card'))
-    assert 'new_card' not in test_deck.current_deck
+    assert 'new_card' not in deck.current_deck
+    deck.add_cards_underneath(['new_card'])
+    assert 'new_card' not in deck.current_deck
+    deck.reset_deck()
+    assert deck.current_deck == ['new_card']
+
+@pytest.fixture
+def board():
+    return Board()
+
+def test_bord_setup(board):
+    assert len(board.road_owner) == len(board.road_data_by_id)
+    for key in range(len(board.road_owner)):
+        assert key in board.road_data_by_id
+
+def test_calculate_all_valid_ways_to_get_grey_road(board):
+    cards = {'green':5,'blue':3,'red':2}
+    two_ways = board.calculate_all_valid_ways_to_get_grey_road(cards, 3)
+    assert len(two_ways) == 2
+    no_way = board.calculate_all_valid_ways_to_get_grey_road(cards, 6)
+    assert no_way is None
+
+
+def test_generate_valid_cards_count_for_path(board):
+
+    got_result = board.generate_valid_cards_count_for_path(4 , 'yellow' , 5 , 0)
+    assert got_result == ('yellow',4)
+
+    not_enough_cards = board.generate_valid_cards_count_for_path(6 , 'yellow' , 5 , 0)
+    assert not_enough_cards is None
+
+    enough_with_jokers = board.generate_valid_cards_count_for_path(6 , 'yellow' , 5 , 1)
+    assert enough_with_jokers == ('yellow',5,'joker',1)
+
+    only_jokers = board.generate_valid_cards_count_for_path(6 , 'yellow' , 0 , 6)
+    assert only_jokers == ('joker', 6)
+
+if __name__ == '__main__':
+    pytest.main()
